9bfba7655f739ef0b368fabbf1b7aa6a5d57eeab
caue96 <cauemateus@hotmail.com>
2022-10-24 14:58:58 -0300

    Assembling form

--- organo/src/components/formulary/index.js
+++ organo/src/components/formulary/index.js
@@ -1,14 +1,30 @@
+import Button from "../button"
+import DropDown from "../dropDown"
 import TextField from "../textField"
 import "./formulary.css"
 
 const Formulary = () => {
+    const times = [
+        "A",
+        "B",
+        "C",
+        "D"
+    ]
+
+    const onSave = (event) => {
+        event.preventDefault()
+        console.log("Salvando")
+    }
+
     return (
         <section className="formulary">
-            <form>
+            <form onSubmit={onSave}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <TextField label="Nome" placeholder="Digite seu nome ..." />
-                <TextField label="Cargo" placeholder="Digite seu cargo ..." />
+                <TextField mandatory={true} label="Nome" placeholder="Digite seu nome ..." />
+                <TextField mandatory={true} label="Cargo" placeholder="Digite seu cargo ..." />
                 <TextField label="Imagem" placeholder="Digite o endereço da imagem ..." />
+                <DropDown mandatory={true} label ="Time" itens={times} />
+                <Button>Criar Card</Button>
             </form>
         </section>
     )
